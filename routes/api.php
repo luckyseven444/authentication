3359d44f112bf06e8427df4080289ba71279b259
Gazi Salah Uddin <gazisalahuddin73@gmail.com>
2025-03-09 03:08:24 +0600

    user can be retrieved by token

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/refresh', [AuthController::class, 'refreshToken']);
+// Route::post('/user', [AuthController::class, 'getUser']);
+Route::post('/user', function(Request $request) {//dd($request->token);
+    return \Auth::user();
+})->middleware('auth:api');
 
 Route::group(['middleware' => ['auth:api']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
